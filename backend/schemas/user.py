3bcb3155d623d9e1f829401a00f177e29d86537f
MinseoKim01 <202513341@pukyong.ac.kr>
2025-11-19 22:19:18 +0900

    main 브랜치 내용 반영 후 내 작업 브랜치 최신화

--- backend/schemas/user.py
+++ backend/schemas/user.py
@@ -7,20 +7,14 @@ class UserBase(BaseModel):
     nickname: str
     selected: int  # 선택된 포켓몬 (max 6이라고 했던 값)
 
-
-class UserCreate(UserBase):
-    email: str
+class UserCreate(UserBase): # 회원가입
     pw: str  # 비밀번호는 생성 시에만 받기
-    nickname: str
 
-class UserLogin(BaseModel):
-    email: EmailStr
+class UserLogin(BaseModel): # 로그인
     pw: str
 
-class UserOut(UserBase):
+class UserOut(UserBase): # 응답
     user_id: int
-    email: str
-    nickname: str
 
     # Pydantic v2에서는 orm_mode -> from_attributes
     class Config:
